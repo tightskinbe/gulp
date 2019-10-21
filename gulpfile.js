48ee3325f57928718d0d344d16c11d06d2017c89
tightskinbe <wowmeupeight@gmail.com>
2019-10-21 13:45:18 -0700

    added eslint

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,28 @@
 const gulp = require("gulp"),
     terser = require("gulp-terser"),
     rename = require("gulp-rename"),
-    browserSync = require("browser-sync");
+    browserSync = require("browser-sync"),
+    eslint = require("gulp-eslint");
 
-
-gulp.task("scripts", function () {
+gulp.task("lint", function () {
     return gulp
         .src("./js/*.js")
-        .pipe(terser())
-        .pipe(rename({ extname: ".min.js" }))
-        .pipe(gulp.dest("./build/js"));
+        .pipe(eslint())
+        .pipe(eslint.format())
+        .pipe(eslint.failAfterError());
 });
+
+
+gulp.task("scripts",
+    gulp.series("lint",
+        function scripts() {
+            return gulp
+                .src("./js/*.js")
+                .pipe(terser())
+                .pipe(rename({ extname: ".min.js" }))
+                .pipe(gulp.dest("./build/js"));
+        })
+);
 
 gulp.task("say_hello", function (done) {
     console.log("Hello!");
@@ -35,6 +47,11 @@ gulp.task('browser-sync', function () {
 
 gulp.watch(["*.html", "build/js/*.js", "css/*.css"])
     .on("change", browserSync.reload);
+
+
+
+
+
 
 
 gulp.task("default", gulp.parallel("browser-sync", "scripts"));
